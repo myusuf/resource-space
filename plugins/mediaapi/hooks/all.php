ca56397135051681bcd61585ee3128ad80c50e26
Franz De Leon <fleo@osidev-imac2.loctest.gov>
2014-01-22 17:43:26 -0500

    added alternative files auto copy from main resource

--- plugins/mediaapi/hooks/all.php
+++ plugins/mediaapi/hooks/all.php
@@ -46,13 +46,40 @@ function HookMediaapiAllAfternewresource($ref)
 
 function HookMediaapiAllUploadfilesuccess($ref)
 {
-
     //var_dump(get_resource_files($ref));die;
-    //todo: do an actual hard copy of resource files from above
-    //$path=get_resource_path($resource, true, "", true, $extension, -1, 1, false, "", $ref);
+    $res_data = get_resource_data($ref);
+    $res_path = get_resource_path($ref, true, "", false, $res_data['file_extension'], -1, 1, false, "");
 
-    $ref     = copy_resource($ref);
     $plfilename = isset($_REQUEST["name"]) ? $_REQUEST["name"] : '';
-    $newfile = add_alternative_file($ref, $plfilename);
-    save_alternative_file($ref, $newfile);
+    //$copied_res       = copy_resource($copied_res);
+    $new_alt_resource = add_alternative_file($ref, $plfilename);
+
+    # Find the path for this resource.
+    $alt_file_path = get_resource_path($ref, true, "", true, $res_data['file_extension'], -1, 1, false, "", $new_alt_resource);
+
+    $result = copy($res_path, $alt_file_path);
+    $sql = '';
+	if ($result==false) {
+		exit("File upload error. Please check the size of the file you are trying to upload.");
+	} else {
+		chmod($alt_file_path, 0777);
+		$file_size = @filesize_unlimited($alt_file_path);
+		$sql.=",file_name='" . escape_check($plfilename) . "',file_extension='" . escape_check($res_data['file_extension']) . "',file_size='" . $file_size . "',creation_date=now()";
+	}
+
+	create_previews($ref, false, $res_data['file_extension'], false, false, $new_alt_resource);
+
+    //resource_log($resource,"b","",$ref . ": " . getvalescaped("name","") . ", " . getvalescaped("description","") . ", " . escape_check($filename));
+	# Save data back to the database.
+	sql_query("
+	   update resource_alt_files set
+	   alt_type='".$res_data['resource_type']."' $sql
+	   where resource='$ref' and ref='$new_alt_resource'
+	");
+
+	# Update disk usage
+	update_disk_usage($new_alt_resource);
+
+
+    //save_alternative_file($ref, $new_resource);
 }
